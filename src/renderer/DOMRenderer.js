6aafa28212db0c48efec2436d7c142bd74e87cda
ManuelOdelain <hello@manuelodelain.com>
2016-04-08 12:16:12 +0200

    DOM renderer: auto background image/size, optional scale factor (for hd screens)

--- src/renderer/DOMRenderer.js
+++ src/renderer/DOMRenderer.js
@@ -1,7 +1,19 @@
 'use strict';
 
-var DOMRenderer = function(element){
+var DOMRenderer = function(element, options){
+  options = options || {};
+
   this.element = element;
+
+  this.scaleFactor = options.scaleFactor || 1;
+
+  if (options.sprite){
+    var spriteWidth = options.sprite.naturalWidth * this.scaleFactor;
+    var spriteHeight = options.sprite.naturalHeight * this.scaleFactor;
+
+    this.element.style.backgroundImage = 'url(' + options.sprite.src + ')';
+    this.element.style.backgroundSize = spriteWidth + 'px ' + spriteHeight + 'px';
+  }
 };
 
 DOMRenderer.prototype.render = function(frame) {
