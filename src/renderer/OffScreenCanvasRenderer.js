4084771988e9620e846de8483a3616fd6287649c
ManuelOdelain <hello@manuelodelain.com>
2016-04-12 15:47:42 +0200

    offscreen canvas renderer: update with alpha/position

--- src/renderer/OffScreenCanvasRenderer.js
+++ src/renderer/OffScreenCanvasRenderer.js
@@ -1,8 +1,18 @@
 'use strict';
 
-var OffScreenCanvasRenderer = function(canvas, sprite){
+var OffScreenCanvasRenderer = function(canvas, sprite, options){
+  options = options || {};
+  
   this.canvas = canvas;
   this.sprite = sprite;
+
+  var defaultOptions = {
+    clearFrame: true
+  };
+
+  for (var optionName in defaultOptions){
+    this[optionName] = typeof options[optionName] !== 'undefined' ? options[optionName] : defaultOptions[optionName];
+  }
   
   this.buffer = document.createElement('canvas');
   this.buffer.width = sprite.width;
@@ -14,13 +24,19 @@ var OffScreenCanvasRenderer = function(canvas, sprite){
   this.context = canvas.getContext('2d');
 };
 
-OffScreenCanvasRenderer.prototype.render = function(frame) {
-  this.context.clearRect(0, 0, frame.width, frame.height);
+OffScreenCanvasRenderer.prototype.render = function(frame, animation) {
+  if (this.clearFrame) this.context.clearRect(0, 0, frame.width, frame.height);
+
+  this.context.globalAlpha = animation.alpha;
   
   this.context.putImageData(
     this.bufferContext.getImageData(frame.x,frame.y,frame.width,frame.height),
+    animation.x,
+    animation.y,
     0,
-    0
+    0,
+    frame.width,
+    frame.height
    );
 };
 
